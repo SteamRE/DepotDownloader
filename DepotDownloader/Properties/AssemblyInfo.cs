63569977ec83d83c6de53feccb172ac850047a61
Nicholas Hastings <nshastings@gmail.com>
2017-02-23 07:32:47 -0500

    Bump version number.

--- DepotDownloader/Properties/AssemblyInfo.cs
+++ DepotDownloader/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany("SteamRE Team")]
 [assembly: AssemblyProduct( "DepotDownloader" )]
-[assembly: AssemblyCopyright("Copyright © SteamRE Team 2016")]
+[assembly: AssemblyCopyright("Copyright © SteamRE Team 2017")]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.*")]
+[assembly: AssemblyVersion("2.2.1.*")]
