16d1e048c4fcc24b21801604a751cd3a951140ca
minecraftitsover90@gmail.com <Connor Jacobs>
2024-02-17 19:10:55 +1030

    revert commit

--- DepotDownloader/CDNClientPool.cs
+++ DepotDownloader/CDNClientPool.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using SteamKit2;
 using SteamKit2.CDN;
 
 namespace DepotDownloader
